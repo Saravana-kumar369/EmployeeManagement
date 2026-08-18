2a678f01569926ad48b02ccc01967aa421334fb1
sk <saravanakumarptk@gmail.com>
2026-08-18 21:30:42 +0530

    added the proper global exception handling and status codes

--- src/main/java/com/example/employee_api/service/EmployeeService.java
+++ src/main/java/com/example/employee_api/service/EmployeeService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.employee_api.Entity.Employee;
 import com.example.employee_api.Repository.EmployeeRepository;
+import com.example.employee_api.exception.EmployeeNotFoundException;
 
 @Service
 public class EmployeeService {
@@ -17,7 +18,11 @@ public class EmployeeService {
     }
     
     public Employee getEmployeeDetails(int id) {
-        return employeeRepository.findById(id).orElse(null);
+        // return employeeRepository.findById(id).orElse(null);
+        // use to throw an exception if employee not found the above line will return null and makes succeed status in controller.    
+        // return employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found with id: " + id));
+        // reduces repeated code and makes it more readable by creating a custom exception class EmployeeNotFoundException.
+        return employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
     }
     public Employee createEmployee(Employee employee) {
     return employeeRepository.save(employee);
